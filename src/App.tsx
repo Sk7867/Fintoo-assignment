b3ade18067f218bb449e9d9985651e1e254c3e45
Sk7867 <siddhesh.kamble7867@gmail.com>
2025-08-08 21:51:32 +0530

    Added new usePortfolio data hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import UserSummaryCard from './components/UserSummaryCard/UserSummaryCard'
@@ -7,39 +7,17 @@ import AssetAllocation from './components/AssetAllocationSection/AssetAllocation
 import HoldingsTable from './components/HoldingsTableSection/HoldingsTable'
 import InsightsSection from './components/InsightsSection/InsightsSection'
 import { snapshots } from './data/portfolioData'
-import type { IAssetAllocation, IHoldings, IInsightsData, IUserData } from './models/user-data.interface'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { usePortfolioData } from './hooks/usePortfolioData'
 const availableDates = Object.keys(snapshots)
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(availableDates[0])
-  const [userData, setUserData] = useState<IUserData>({} as IUserData)
-  const [assetAllocation, setAssetAllocation] = useState([] as IAssetAllocation[])
-  const [insightsData, setInsightsData] = useState({} as IInsightsData)
-  const currentSnapshot = snapshots[selectedDate as keyof typeof snapshots]
-  const [holdingsData, setHoldingsData] = useState([] as IHoldings[])
-  useEffect(() => {
-    if (currentSnapshot) {
-      const user = currentSnapshot.user as IUserData
-      const assetAllocations = currentSnapshot.assetAllocation as IAssetAllocation[]
-      const holdingsData = currentSnapshot.holdings as IHoldings[]
-      const insights = currentSnapshot.insights as IInsightsData
-      console.log('Current Snapshot:', currentSnapshot)
-      setUserData(user)
-      setAssetAllocation(assetAllocations)
-      setHoldingsData(holdingsData)
-      setInsightsData(insights)
-    }
-  }, [selectedDate, currentSnapshot])
+  const { data } = usePortfolioData(selectedDate)
 
-  // useEffect(() => {
-  //   const currentSnapshot = snapshots[selectedDate]
-  //   if (currentSnapshot) {
-  //     setUserData(currentSnapshot.user)
-  //   }
-  //   return () => { }
-  // }, [selectedDate])
-
+  if (!data) {
+    return <div className="container mx-auto px-4 py-6">No Portfolio Data Available</div>
+  }
 
   return (
     <ThemeProvider>
@@ -47,10 +25,10 @@ function App() {
         <Header selectedDate={selectedDate} setSelectedDate={setSelectedDate} datesArray={availableDates} />
 
         <div className='container mx-auto px-4 py-6 space-y-6'>
-          <UserSummaryCard userData={userData} />
-          <AssetAllocation assetAllocation={assetAllocation} />
-          <HoldingsTable holdingsData={holdingsData} />
-          <InsightsSection insights={insightsData} />
+          <UserSummaryCard userData={data.userData} />
+          <AssetAllocation assetAllocation={data.assetAllocation} />
+          <HoldingsTable holdingsData={data.holdingsData} />
+          <InsightsSection insights={data.insightsData} />
         </div>
 
       </div>
